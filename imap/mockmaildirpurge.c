3c5680204429c7d5167b8c5d935b4a74dd2b51b2
Sam Varshavchik <mrsam@courier-mta.com>
2022-01-22 19:40:17 -0500

    Add additional tests.
    Test for IMAP_EMPTYTRASH parsing.

---
+++ imap/mockmaildirpurge.c
@@ -0,0 +1,22 @@
+/*
+** Copyright 20212 Double Precision, Inc.
+** See COPYING for distribution information.
+*/
+
+#if	HAVE_CONFIG_H
+#include	"config.h"
+#endif
+
+#include	"maildir/maildirmisc.h"
+#include	<stdio.h>
+
+void maildir_purgetmp(const char *dir)
+{
+	printf("maildir_purgetmp: %s\n", dir);
+}
+
+void maildir_purge(const char *dir,
+		   unsigned n)
+{
+	printf("maildir_purge: %s (%u)\n", dir, n);
+}
