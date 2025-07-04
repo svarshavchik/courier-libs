7886ea354135273fa9f20439f003c8f1d6f5b903
Sam Varshavchik <mrsam@courier-mta.com>
2025-07-03 22:05:35 -0400

    Compile encodeautodetect as C++

--- rfc822/encodeautodetect.C
+++ rfc822/encodeautodetect.C
@@ -10,8 +10,14 @@
 #include	<stdlib.h>
 #include	<courier-unicode.h>
 
+namespace {
+#if 0
+}
+#endif
+
+template<typename GETC>
 static const char *libmail_encode_autodetect(int use7bit,
-					     int (*func)(void *), void *arg,
+					     GETC &&getc,
 					     int *binaryflag)
 {
 	int	l=0;
@@ -24,7 +30,7 @@ static const char *libmail_encode_autodetect(int use7bit,
 	if (binaryflag)
 		*binaryflag=0;
 
-	while ((c = (*func)(arg)) != EOF)
+	while ((c = getc()) != EOF)
 	{
 		unsigned char ch= (unsigned char)c;
 
@@ -67,32 +73,9 @@ static const char *libmail_encode_autodetect(int use7bit,
 	return bit8cnt ? "8bit":"7bit";
 }
 
-struct file_info {
-	FILE *fp;
-	off_t pos;
-	off_t end;
-};
-
-static int read_file(void *arg)
+#if 0
 {
-int c;
-struct file_info *fi = (struct file_info *)arg;
-	if (fi->end >= 0 && fi->pos > fi->end)
-		return EOF;
-	c = getc(fi->fp);
-	fi->pos++;
-	return c;
-}
-
-static int read_string(void * arg)
-{
-int c;
-unsigned char **strp = (unsigned char **)arg;
-	if (**strp == 0)
-		return EOF;
-	c = (int)**strp;
-	(*strp)++;
-	return c;
+#endif
 }
 
 const char *libmail_encode_autodetect_fp(FILE *fp, int use7bit,
@@ -106,10 +89,9 @@ const char *libmail_encode_autodetect_fpoff(FILE *fp, int use7bit,
 					    off_t start_pos, off_t end_pos,
 					    int *binaryflag)
 {
-struct file_info fi;
-off_t orig_pos = ftell(fp);
-off_t pos = orig_pos;
-const char *rc;
+	off_t orig_pos = ftell(fp);
+	off_t pos = orig_pos;
+	const char *rc;
 
 	if (start_pos >= 0)
 	{
@@ -119,13 +101,18 @@ const char *rc;
 			pos = start_pos;
 	}
 
-	fi.fp = fp;
-	fi.pos = pos;
-	fi.end = end_pos;
+	rc = libmail_encode_autodetect(use7bit,
+				       [&]
+				       {
+					       if (end_pos >= 0 &&
+						   pos > end_pos)
+						       return EOF;
 
-	rc = libmail_encode_autodetect(use7bit, &read_file, &fi,
+					       ++pos;
+					       return getc(fp);
+				       },
 				       binaryflag);
-  
+
 	if (fseek(fp, orig_pos, SEEK_SET) == (off_t)-1)
 		return NULL;
 	return rc;
@@ -133,6 +120,15 @@ const char *rc;
 
 const char *libmail_encode_autodetect_buf(const char *str, int use7bit)
 {
-	return libmail_encode_autodetect(use7bit, &read_string, &str,
+	return libmail_encode_autodetect(use7bit,
+					 [&]
+					 ()-> int
+					 {
+						 if (!*str)
+							 return EOF;
+						 return static_cast<
+							 unsigned char
+							 >(*str++);
+					 },
 					 NULL);
 }
